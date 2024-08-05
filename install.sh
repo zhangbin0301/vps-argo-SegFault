8e60823db69c9842c2a970e52bf5472ae132774e
zhangbin0301 <zhangbin0301@qq.com>
2024-08-05 08:24:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,6 @@ read UUID
 UUID=${UUID:-"ea4909ef-7ca6-4b46-bf2e-6c07896ef338"}
 VPATH='vls'
 
-# 自定义哪吒探针下载，也可默认0.18.2之前旧版本
-export NEZ_AMD_URL=${NEZ_AMD_URL:-'https://raw.githubusercontent.com/kahunama/myfile/main/nezha/nezha-agent'}
-export NEZ_ARM_URL=${NEZ_ARM_URL:-'https://raw.githubusercontent.com/kahunama/myfile/main/nezha/nezha-agent(arm)'}
 
 
 # 设置订阅上传地址
@@ -146,6 +143,11 @@ export SERVER_IP='$SERVER_IP'
 export UUID='$UUID'
 export VPATH='$VPATH'
 export SUB_URL='$SUB_URL'
+
+# 自定义哪吒探针下载，也可默认0.18.2之前旧版本
+export NEZ_AMD_URL=${NEZ_AMD_URL:-'https://raw.githubusercontent.com/kahunama/myfile/main/nezha/nezha-agent'}
+export NEZ_ARM_URL=${NEZ_ARM_URL:-'https://raw.githubusercontent.com/kahunama/myfile/main/nezha/nezha-agent(arm)'}
+
 
 if command -v curl &>/dev/null; then
     DOWNLOAD_CMD="curl -sL"
